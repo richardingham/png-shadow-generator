e94d1bcd2b2d737c58a79984706efb4a8b66e30f
arphire <arphire@d0c89e98-80f2-11dd-88bd-cff828a63736>
2008-09-12 19:00:54 +0000

    Blank image added, originally omitted. Obfuscated version limited to 80 chars / line.

--- trunk/shadow/sh_basic_obfuscated.php
+++ trunk/shadow/sh_basic_obfuscated.php
@@ -25,10 +25,20 @@ ini_set('error_reporting', 0);
 header('Content-Type: image/png');
 header("Expires: " . gmdate("D, d M Y H:i:s", time() + 30000000) . " GMT");
 
-$a=(float)(isset($_GET['a'])?$_GET['a']:8);$b=(float)(isset($_GET['b'])?$_GET['b']:0.35);$k=chr(99);list($j,$B,$x,$I,$l,$T,$O,$o,$f,$X,$t,$Y,$W,$H)=explode($k,'0c255cimagecdc127csinczcarra'
-.'y_mapc_GETcmincstrtrcureatebuolorallouatealphabfilledreutanglebsetpixelbpngbdestroycwch');list($o,$O,$Y,$I,$t,$_)=$o($I,explode($O,$t($Y,'ub',"$k$O")));$W=(int)((isset(${$f}[$W])?${$f}[$W
-]:$B)+$a);$H=(int)((isset(${$f}[$H])?${$f}[$H]:$B)+$a);$S=(int)(($a/$B[$j])+($X($a,(float)($W-$a))/($B[$j]*$B[$j])));$O($i=$o($W,$H),$B,$B,$B,$l);$u=$l-((int)($b*$l));$C[$u]=$O($i,$j,$j,$j,
-$u);$Y($i,$S,$S,$W-$S,$H-$S,$C[$u]);function c($_,$t){global $C,$O,$i,$j,$l;$_=$l-((int)($t*$_*$t*$l*$t));return((isset($C[$_]))?$C[$_]:$C[$_]=$O($i,$j,$j,$j,$_));}function d($_){global $x;
-return"$x$_";}for($n=$j,$f=$l[$j]/(float)$S;$n<$S;++$n){$$k=$k($b,$T($n*($f*M_PI_2)));for($P=$S,$M=$W-$S;$P<$M;++$P){$I($i,$P,$n,$c);$I($i,$P,$H-$n,$c);}for($p=$S,$M=$H-$S;$p<$M;++$p){$I($i
-,$n,$p,$c);$I($i,$W-$n,$p,$c);}}for($n=$j,$f=$l[$j]/(float)$S;$n<=$S;++$n){for($S,$N=$j,$F=$l[$j]/(float)$S;$N<=$S;++$N){$$k=$k($b,$T($n*($f*M_PI_2))*$T($N*$F*M_PI_2));$I($i,$n,$N,$c);$I($i
-,$W-$n,$N,$c);$I($i,$n,$H-$N,$c);$I($i,$W-$n,$H-$N,$c);}}$k=dirname(__FILE__)."/$a-$b/".($W-$a).'/'.($H-$a);if(!file_exists($k))mkdir($k,0777,true);$t($i,$k.chr(47).'sh.png');$t($i);$_($i);
+$a=(float)(isset($_GET['a'])?$_GET['a']:(8));$b=(float)(isset($_GET['b'])?$_GET
+['b']:.35);$k=chr(9*11);list($j,$B,$x,$I,$l,$T,$O,$o,$f,$X,$t,$Y,$W,$H)=explode
+($k,'0c255cimagecdc127csinczcarray_mapc_GETcmincstrtrcureatebuolorallouatealph'
+.'abfilledreutanglebsetpixelbpngbdestroycwch');list($o,$O,$Y,$I,$t,$_,$Q)=$o($I
+,explode($O,$t($Y,'ub',"$k$O")));$W=(int)(((isset(${$f}[$W])?${$f}[$W]:(int)$B)
++$a));$H=(int)((isset(${$f}[$H])?${$f}[$H]:$B)+$a);$S=(int)(($a/$B[$j])+($X($a,
+(float)($W-$a))/($B[$j]*$B[$j])));$O($i=$o($W,$H),$B,$B,$B,$l);$u=$l-((int)($b*
+$l));$C[$u]=$O($i,$j,$j,$j,$u);$Y($i,$S,$S,$W-$S,$H-$S,$C[$u]);function c($_,$t
+){global $C,$O,$i,$j,$l;$_=$l-((int)($t*$_*$t*$l*$t));return((isset($C[$_]))?$C
+[$_]:$C[$_]=$O($i,$j,$j,$j,$_));}function d($_){global $x;return"$x$_";}for($n=
+$j,$f=$l[$j]/(float)$S;$n<$S;++$n){${$k}=$k($b,$T($n*($f*M_PI_2)));for($P=$S,$M
+=$W-$S;$P<$M;++$P){$I($i,$P,$n,$c);$P;$I($i,$P,$H-$n,$c);}for($p=$S,$M=$H-$S;$p
+<$M;++$p){$I($i,$n,$p,$c);$I($i,$W-$n,$p,$c);}}for($n=$j,$f=$l[$j]/(float)$S;$n
+<=$S;++$n){for($S,$N=$j,$F=$l[$j]/((float)$S);$N<=$S;++$N){$$k=$k($b,$T($n*($f*
+M_PI_2))*$T($N*$F*M_PI_2));$I($i,$n,$N,$c);$I($i,$W-$n,$N,$c);$I($i,$n,$H-$N,$c
+);$I($i,$W-$n,$H-$N,$c);}}$k=dirname(__FILE__)."/$a-$b/".($W-$a).'/'.($H-$a);if
+(!file_exists($k))mkdir($k,0777,true);$t($i,$k.chr(47).'sh.png');$t($i);$_($i);
